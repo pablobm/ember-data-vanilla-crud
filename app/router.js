fdba460d7a32c0f0a4a6f4ac4f4dd1a9cf995dff
Pablo Brasero <pablo@pablobm.com>
2016-04-03 16:28:11 +0100

    `index` is always implicit

--- app/router.js
+++ app/router.js
@@ -7,7 +7,6 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('lines', function() {
-    this.route('index', {path: '/'});
     this.route('new');
     this.route('show', {path: ':id'});
     this.route('edit', {path: ':id/edit'});
